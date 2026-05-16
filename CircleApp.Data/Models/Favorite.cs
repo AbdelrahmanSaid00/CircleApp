e412d9dc124c6ed03a2399ea46f7ad0ce598d420
Abdalrahman <30509151402792@sci.asu.edu.eg>
2026-05-17 01:27:17 +0300

    Implementing Post Favorites

---
+++ CircleApp.Data/Models/Favorite.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CircleApp.Data.Models
+{
+    public class Favorite
+    {
+        public int Id { get; set; }
+        public DateTime DateCreate { get; set; }
+        public int postId { get; set; }
+        public int userId { get; set; }
+        //Navigation properties
+        public Post Post { get; set; }
+        public User User { get; set; }
+
+    }
+}
